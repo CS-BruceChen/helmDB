b260becb097686a9537da74232f5970460d9755d
csf <9033986+WHUBruceChen@user.noreply.gitee.com>
2024-11-16 13:37:49 +0800

    完成了基本布局

--- MM1123-frontend/src/App.vue
+++ MM1123-frontend/src/App.vue
@@ -1,85 +1,42 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main>
+    <div class="sql-container">div1</div>
+    <div class="res-container">
+      <div class="vector-canvas">div2</div>
+      <div class="relation-terminal">div3</div>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+main {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100%;
+  display: flex;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.sql-container {
+  flex: 1;
+  height: 100%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.res-container {
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.vector-canvas {
+  flex: 1;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.relation-terminal {
+  flex: 1;
 }
 </style>
